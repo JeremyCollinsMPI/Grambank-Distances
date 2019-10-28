95e150b7f0a19a1358fbee165e54471e8a726e5f
Jeremy Collins <jeremyccollins@gmail.com>
2019-10-28 16:44:25 +0800

    further work

--- Main.py
+++ Main.py
@@ -1,10 +1,17 @@
 import tensorflow as tf
 # from MakeArrays import *
 import numpy as np
+import math
 
 input_array = np.load('input_array.npy')
+
+
 output_array = np.load('output_array.npy')
 null_layer = np.load('null_layer.npy')
+
+
+
+
 # input_array_shape = np.shape(input_array)
 # output_array_shape = np.shape(output_array)
 # null_layer_shape = np.shape(null_layer)
@@ -31,15 +38,15 @@ def moose(x):
 
 class Model(object):
   def __init__(self):
-    self.distances = tf.Variable(np.ones(distances_shape), dtype=tf.float32)
-    self.stabilities = tf.Variable(np.zeros(stabilities_shape), dtype=tf.float32)
-    self.biases = tf.Variable(np.zeros(biases_shape), dtype=tf.float32)
-#     self.distances = tf.random.uniform(distances_shape, minval=0, maxval=1, dtype=tf.float32)
-#     self.stabilities = tf.random.uniform(stabilities_shape, minval=-3, maxval=-0.01, dtype=tf.float32)
-#     self.biases = tf.random.uniform(biases_shape, minval=0, maxval=1, dtype=tf.float32)
+    self.distances = tf.Variable(tf.random.uniform(distances_shape, minval=0.5, maxval=1, dtype=tf.float32))
+    self.stabilities = tf.Variable(tf.random.uniform(stabilities_shape, minval=-1, maxval=0, dtype=tf.float32))
+    self.biases = tf.Variable(tf.random.uniform(biases_shape, minval=0, maxval=0.2, dtype=tf.float32))
+ 
+  def make_batched_distances(self, range1, range2):
+    self.batched_distances = tf.Variable(self.distances[range1:range2])
  
   def __call__(self, input_placeholder, null_layer_placeholder):
-    step1 = tf.matmul(self.distances, self.stabilities) + 3
+    step1 = tf.matmul(self.batched_distances, self.stabilities) + 3
     step2 = tf.math.sigmoid(step1)
     one = tf.constant(np.array([1]), dtype=tf.float32)
     step3 = tf.math.abs(tf.math.subtract(tf.math.subtract(one, input_placeholder), step2))
@@ -48,41 +55,110 @@ class Model(object):
     prediction = tf.math.multiply(step5, null_layer_placeholder)
     return prediction
 
+  def update_distances(self, range1, range2):
+    self.distances[range1:range2].assign(self.batched_distances)
+'''
+     need to check that this is correct
+'''
+
 def loss(predicted_y, target_y):
-  return tf.keras.losses.BinaryCrossentropy(predicted_y, target_y)
+  bce = tf.keras.losses.BinaryCrossentropy()
+  return bce(predicted_y, target_y)
 
 def train(model, input_array, null_layer, output_array, learning_rate):
   with tf.GradientTape() as t:
     current_loss = loss(model(input_array, null_layer), output_array)
-  d_distances, d_stabilities, d_biases = t.gradient(current_loss, [model.distances, model.stabilies, model.biases])
-  model.distances.assign_sub(learning_rate * d_distances)
+  d_distances, d_stabilities, d_biases = t.gradient(current_loss, [model.batched_distances, model.stabilities, model.biases])
+  model.batched_distances.assign_sub(learning_rate * d_distances)
   model.stabilities.assign_sub(learning_rate * d_stabilities)
   model.biases.assign_sub(learning_rate * d_biases)
 
 
 model = Model()
 
-# epochs = range(2)
-# 
-# for epoch in epochs:
+batch_size = 30000
+number_of_batches = int(math.ceil(len(input_array) / batch_size))
+
+epochs = range(50)
+
+
+for epoch in epochs:
+  total_loss = 0
+  print('Epoch ' + str(epoch))
+  for i in range(number_of_batches):
+    range1 = i * batch_size
+    range2 = min(batch_size * (i+1), len(input_array))
+    batched_input_array = input_array[range1:range2]
+    batched_null_layer = null_layer[range1:range2]
+    batched_output_array = output_array[range1:range2]
+    model.make_batched_distances(range1, range2)
+    current_loss = loss(model(batched_input_array, batched_null_layer), batched_output_array)
+    total_loss = total_loss + current_loss.numpy()
+    train(model, batched_input_array, batched_null_layer, batched_output_array, learning_rate=0.005)
+    model.update_distances(range1, range2)
+  print(model.stabilities.numpy())
+  print(total_loss)
+
+'''
+alternative implementation which loads from files:
+
+def save_batches(batch_size):
+  number_of_batches = int(math.ceil(len(input_array) / batch_size))
+  for i in range(number_of_batches):
+    range1 = i * batch_size
+    range2 = min(batch_size * (i+1), len(input_array))  
+    batched_input_array = input_array[range1:range2]
+    batched_null_layer = null_layer[range1:range2]
+    batched_output_array = output_array[range1:range2]
+    np.save('input_array_batch_' + batch_size + '_' + str(i) + '.npy', batched_input_array)
+    np.save('null_layer_batch_' + batch_size + '_' + str(i) + '.npy', batched_null_layer)
+    np.save('output_array_batch_' + batch_size + '_' + str(i) + '.npy', batched_output_array)
+
+model = Model()
+
+batch_size = 1000
+number_of_batches = int(math.ceil(len(input_array) / batch_size))
+epochs = range(2)
+for epoch in epochs:
+  for i in range(number_of_batches):
+    print('moose')
+    range1 = i * batch_size
+    range2 = min(batch_size * (i+1), len(input_array))
+    try:
+      batched_input_array = np.load('input_array_batch_' + batch_size + '_' + str(i) + '.npy')
+      batched_null_layer = np.load('null_layer_batch' + batch_size + '_' + str(i) + '.npy')
+      batched_output_array = np.load('output_array_batch_' + batch_size + '_' + str(i) + '.npy')
+    except:
+      save_batches(batch_size)
+      batched_input_array = np.load('input_array_batch_' + batch_size + '_' + str(i) + '.npy')
+      batched_null_layer = np.load('null_layer_batch' + batch_size + '_' + str(i) + '.npy')
+      batched_output_array = np.load('output_array_batch_' + batch_size + '_' + str(i) + '.npy')
+    model.make_batched_distances(range1, range2)
+    current_loss = loss(model(batched_input_array, batched_null_layer), batched_output_array)
+    train(model, batched_input_array, batched_null_layer, batched_output_array, learning_rate=0.1)
+    model.update_distances(range1, range2)
+'''
+
+    
 #   print('moose')
 #   current_loss = loss(model(input_array, null_layer), output_array)
 #   train(model, input_array, null_layer, output_array, learning_rate=0.1)
 #   print(current_loss)
   
 
-# 
-# steps = 10000
-# learn_rate = 0.03
-# train_step = tf.train.GradientDescentOptimizer(learn_rate).minimize(loss)	
-# init = tf.initialize_all_variables()
-# sess = tf.Session()
-# sess.run(init)
+'''
+how do i save and load from pickle files?
+take input_array; take the batch_size; 
+then for i in range(number_of_batch_sizes)
+take the slice of the array (as above)
+then save it as input_array_batch_ + str(i) + '.npy'
+
+then in the training loop, instead of taking a slice,
+for in range(number_of_batches)
+batched_input_array = np.load('input_array_batch_' + str(i) + '.npy')
 
 
-# for i in xrange(steps):
-#   feed = {x: X, y_: target}
-#   sess.run(train_step, feed_dict = feed)
+'''
 
 
 '''
